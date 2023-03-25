164a2e93b16afcb0a91bf7ad389b067d8d91ea1f
JACYI <718980173@qq.com>
2023-03-25 17:46:41 +0800

    Add buffer pool manager and UT

--- src/buffer/buffer_pool_manager_instance.cpp
+++ src/buffer/buffer_pool_manager_instance.cpp
@@ -22,37 +22,198 @@ BufferPoolManagerInstance::BufferPoolManagerInstance(size_t pool_size, DiskManag
     : pool_size_(pool_size), disk_manager_(disk_manager), log_manager_(log_manager) {
   // we allocate a consecutive memory space for the buffer pool
   pages_ = new Page[pool_size_];
-  page_table_ = new ExtendibleHashTable<page_id_t, frame_id_t>(bucket_size_);
-  replacer_ = new LRUKReplacer(pool_size, replacer_k);
+  replacer_ = std::make_unique<LRUKReplacer>(pool_size, replacer_k);
 
   // Initially, every page is in the free list.
   for (size_t i = 0; i < pool_size_; ++i) {
     free_list_.emplace_back(static_cast<int>(i));
   }
-
-  // TODO(students): remove this line after you have implemented the buffer pool manager
-  throw NotImplementedException(
-      "BufferPoolManager is not implemented yet. If you have finished implementing BPM, please remove the throw "
-      "exception line in `buffer_pool_manager_instance.cpp`.");
 }
 
 BufferPoolManagerInstance::~BufferPoolManagerInstance() {
   delete[] pages_;
-  delete page_table_;
-  delete replacer_;
 }
 
-auto BufferPoolManagerInstance::NewPgImp(page_id_t *page_id) -> Page * { return nullptr; }
+auto BufferPoolManagerInstance::NewPgImp(page_id_t *page_id) -> Page * {
+  latch_.lock();
+  frame_id_t free_frame;
 
-auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * { return nullptr; }
+  if(!free_list_.empty()) {
+    // Get free frame_id_t
+    free_frame = free_list_.front();
+    free_list_.pop_front();
 
-auto BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) -> bool { return false; }
+  } else {
+    // Not exists free page, replacer victim a LRU page
+    if (!replacer_->Evict(&free_frame)) {
+      // No frames can be evicted.
+      latch_.unlock();
+      return nullptr;
+    }
 
-auto BufferPoolManagerInstance::FlushPgImp(page_id_t page_id) -> bool { return false; }
+    // If page is dirty, then write it to the disk first
+    if (pages_[free_frame].is_dirty_) {
+      disk_manager_->WritePage(pages_[free_frame].page_id_,
+                               pages_[free_frame].data_);
+      pages_[free_frame].is_dirty_ = false;
+    }
 
-void BufferPoolManagerInstance::FlushAllPgsImp() {}
+    // Reset the memory and the metadata
+    //    auto rpc_page = pages_[free_frame];
+    page_table_.erase(pages_[free_frame].page_id_);
+    pages_[free_frame].ResetMemory();
+    pages_[free_frame].page_id_ = INVALID_PAGE_ID;
+    pages_[free_frame].pin_count_ = 0;
+  }
 
-auto BufferPoolManagerInstance::DeletePgImp(page_id_t page_id) -> bool { return false; }
+  page_id_t new_page_id = AllocatePage();
+  // Add new page_id to pages
+  pages_[free_frame].page_id_ = new_page_id;
+  pages_[free_frame].pin_count_ = 1;   // At least one thread call this func
+
+  // Establish the relation between page and frame in page table
+  page_table_[new_page_id] = free_frame;
+  // Pin the frame
+  replacer_->RecordAccess(free_frame);
+  replacer_->SetEvictable(free_frame, false);
+  *page_id = new_page_id;
+  latch_.unlock();
+
+  return &pages_[free_frame];
+}
+
+auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {latch_.lock();
+  Page * target_page;
+  // Search for page_id in the buffer pool
+  if(page_table_.count(page_id) != 0) {
+    auto target_frame = page_table_[page_id];
+    target_page = &pages_[target_frame];
+    target_page->pin_count_++;
+    replacer_->RecordAccess(target_frame);
+    latch_.unlock();
+    return target_page;
+  }
+
+  // If not found,
+  // ----------------- pick a replacement frame ---------------------
+  frame_id_t rpc_frame;
+  if(!free_list_.empty()) {
+    rpc_frame = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    if (!replacer_->Evict(&rpc_frame)) {
+      latch_.unlock();
+      return nullptr;
+    }
+    if (pages_[rpc_frame].is_dirty_) {
+      // Write to disk
+      disk_manager_->WritePage(pages_[rpc_frame].page_id_, pages_[rpc_frame].data_);
+      pages_[rpc_frame].is_dirty_ = false;
+    }
+    pages_[rpc_frame].ResetMemory();
+    pages_[rpc_frame].page_id_ = INVALID_PAGE_ID;
+    pages_[rpc_frame].is_dirty_ = false;
+    pages_[rpc_frame].pin_count_ = 0;
+    page_table_.erase(rpc_frame);
+  }
+  pages_[rpc_frame].page_id_ = page_id;
+  pages_[rpc_frame].pin_count_ = 1;
+  page_table_[page_id] = rpc_frame;
+  replacer_->RecordAccess(rpc_frame);
+  replacer_->SetEvictable(rpc_frame, false);
+  // ------------------------------------------------------------
+
+  // Read the page from the disk
+  disk_manager_->ReadPage(page_id, pages_[rpc_frame].data_);
+  latch_.unlock();
+  return &pages_[rpc_frame];
+}
+
+auto BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) -> bool {latch_.lock();
+  // Page not found
+  if(page_table_.count(page_id) == 0) {
+    latch_.unlock();
+    return false;
+  }
+  // pin_count less or equal than zero
+  auto unpin_frame = page_table_[page_id];
+  if(pages_[unpin_frame].pin_count_ <= 0) {
+    latch_.unlock();
+    return false;
+  }
+
+  // Decrease the pin_count
+  pages_[unpin_frame].pin_count_--;
+  if(pages_[unpin_frame].pin_count_ == 0) {
+    // Firstly decrease to zero, should be set for evictable
+    replacer_->SetEvictable(unpin_frame, true);
+  }
+  // Set dirty flag
+  if(is_dirty) {
+    pages_[unpin_frame].is_dirty_ = true;
+  }
+
+  latch_.unlock();
+  return true;
+}
+
+auto BufferPoolManagerInstance::FlushPgImp(page_id_t page_id) -> bool {
+  latch_.lock();
+  if(page_table_.count(page_id) == 0) {
+    // Page not found
+    latch_.unlock();
+    return false;
+  }
+  // Flush to disk
+  auto target_frame = page_table_[page_id];
+  if(pages_[target_frame].page_id_ == INVALID_PAGE_ID) {
+    // Invalid page
+    latch_.unlock();
+    return false;
+  }
+  // Write to disk
+  disk_manager_->WritePage(page_id, pages_[target_frame].data_);
+  pages_[target_frame].is_dirty_ = false;
+  latch_.unlock();
+  return true;
+
+}
+
+void BufferPoolManagerInstance::FlushAllPgsImp() {
+  latch_.lock();
+  // Flush all the pages in buffer pool
+  for(int i=0; i<static_cast<int>(pool_size_); i++) {
+    FlushPage(pages_[i].page_id_);
+  }
+  latch_.unlock();
+}
+
+auto BufferPoolManagerInstance::DeletePgImp(page_id_t page_id) -> bool {
+  latch_.lock();
+  if(page_table_.count(page_id) == 0){
+    latch_.unlock();
+    return true;
+  }
+  auto delete_frame = page_table_[page_id];
+  if(pages_[delete_frame].pin_count_ > 0) {
+    // Pinned
+    latch_.unlock();
+    return false;
+  }
+
+  // Stop tracking and add delete frame to free list
+  replacer_->Remove(delete_frame);
+  page_table_.erase(page_id);
+  free_list_.push_back(delete_frame);
+  // Reset the data
+  pages_[delete_frame].page_id_ = INVALID_PAGE_ID;
+  pages_[delete_frame].ResetMemory();
+  pages_[delete_frame].pin_count_ = 0;
+  pages_[delete_frame].is_dirty_ = false;
+
+  latch_.unlock();
+  return true;
+}
 
 auto BufferPoolManagerInstance::AllocatePage() -> page_id_t { return next_page_id_++; }
 
--- src/include/buffer/buffer_pool_manager_instance.h
+++ src/include/buffer/buffer_pool_manager_instance.h
@@ -54,8 +54,6 @@ class BufferPoolManagerInstance : public BufferPoolManager {
 
  protected:
   /**
-   * TODO(P1): Add implementation
-   *
    * @brief Create a new page in the buffer pool. Set page_id to the new page's id, or nullptr if all frames
    * are currently in use and not evictable (in another word, pinned).
    *
@@ -73,8 +71,6 @@ class BufferPoolManagerInstance : public BufferPoolManager {
   auto NewPgImp(page_id_t *page_id) -> Page * override;
 
   /**
-   * TODO(P1): Add implementation
-   *
    * @brief Fetch the requested page from the buffer pool. Return nullptr if page_id needs to be fetched from the disk
    * but all frames are currently in use and not evictable (in another word, pinned).
    *
@@ -91,8 +87,6 @@ class BufferPoolManagerInstance : public BufferPoolManager {
   auto FetchPgImp(page_id_t page_id) -> Page * override;
 
   /**
-   * TODO(P1): Add implementation
-   *
    * @brief Unpin the target page from the buffer pool. If page_id is not in the buffer pool or its pin count is already
    * 0, return false.
    *
@@ -106,8 +100,6 @@ class BufferPoolManagerInstance : public BufferPoolManager {
   auto UnpinPgImp(page_id_t page_id, bool is_dirty) -> bool override;
 
   /**
-   * TODO(P1): Add implementation
-   *
    * @brief Flush the target page to disk.
    *
    * Use the DiskManager::WritePage() method to flush a page to disk, REGARDLESS of the dirty flag.
@@ -119,15 +111,11 @@ class BufferPoolManagerInstance : public BufferPoolManager {
   auto FlushPgImp(page_id_t page_id) -> bool override;
 
   /**
-   * TODO(P1): Add implementation
-   *
    * @brief Flush all the pages in the buffer pool to disk.
    */
   void FlushAllPgsImp() override;
 
   /**
-   * TODO(P1): Add implementation
-   *
    * @brief Delete a page from the buffer pool. If page_id is not in the buffer pool, do nothing and return true. If the
    * page is pinned and cannot be deleted, return false immediately.
    *
@@ -140,12 +128,11 @@ class BufferPoolManagerInstance : public BufferPoolManager {
    */
   auto DeletePgImp(page_id_t page_id) -> bool override;
 
+ private:
   /** Number of pages in the buffer pool. */
   const size_t pool_size_;
   /** The next page id to be allocated  */
   std::atomic<page_id_t> next_page_id_ = 0;
-  /** Bucket size for the extendible hash table */
-  const size_t bucket_size_ = 4;
 
   /** Array of buffer pool pages. */
   Page *pages_;
@@ -154,9 +141,9 @@ class BufferPoolManagerInstance : public BufferPoolManager {
   /** Pointer to the log manager. Please ignore this for P1. */
   LogManager *log_manager_ __attribute__((__unused__));
   /** Page table for keeping track of buffer pool pages. */
-  ExtendibleHashTable<page_id_t, frame_id_t> *page_table_;
+  std::unordered_map<page_id_t, frame_id_t> page_table_;
   /** Replacer to find unpinned pages for replacement. */
-  LRUKReplacer *replacer_;
+  std::unique_ptr<LRUKReplacer> replacer_;
   /** List of free frames that don't have any pages on them. */
   std::list<frame_id_t> free_list_;
   /** This latch protects shared data structures. We recommend updating this comment to describe what it protects. */
@@ -175,7 +162,5 @@ class BufferPoolManagerInstance : public BufferPoolManager {
   void DeallocatePage(__attribute__((unused)) page_id_t page_id) {
     // This is a no-nop right now without a more complex data structure to track deallocated pages
   }
-
-  // TODO(student): You may add additional private members and helper functions
 };
 }  // namespace bustub
